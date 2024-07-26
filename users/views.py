60e762cf8c7026923d4e5d1958bb258477a097cb
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-07-26 22:47:22 +0500

    Добавлена верификация по почте

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+import secrets
+
+from django.conf import settings
+from django.contrib.auth import login
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
 from users.forms import UserRegisterForm
@@ -12,4 +17,32 @@ class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('main:mailings')
+    success_url = reverse_lazy('users:email_confirm')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email_confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения почты на сервисе SendEmail перейдите по ссылке {url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+
+def email_verification_message(request):
+    return render(request, 'users/email_verification_message.html')
+
+
+def email_verification_token(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    login(request, user)
+    return redirect(reverse('main:mailings'))
