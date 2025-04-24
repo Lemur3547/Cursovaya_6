8622a85232a42e684838b6376170ebf0e555ad68
Emmanuil Galihanov <emikgal2507@gmail.com>
2025-04-24 23:36:47 +0500

    Urtk version

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.cache import cache_page
-
-from blog.views import BlogListView, BlogDetailView
-
-urlpatterns = [
-    path('', cache_page(60)(BlogListView.as_view()), name='index'),
-    path('post/<int:pk>', cache_page(60)(BlogDetailView.as_view()), name='post'),
-]
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('main.urls', 'main',),  namespace='main')),
-    path('users/', include(('users.urls', 'users',),  namespace='users')),
-    path('blog/', include(('blog.urls', 'blog',),  namespace='blog')),
+    path('users/', include(('users.urls', 'users',),  namespace='users'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
